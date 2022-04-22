a1058cd602a6d0f67e977d8c3a0ec0804e182ef5
monseeligio <monserrateligio@gmail.com>
2022-04-21 20:52:16 -0500

    paso 7: agregando route //localhost:3000/explorers/monseeligo y recibiendo parametros

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ const port =3000
 app.get('/',(req,res)=>{
     res.send("Hola a todos!!!")
 })
+//localhost:3000/launchx
+app.get('/launchx',(req,res)=>{
+    res.send("Bienvenidos a launchx!!!")
+})
+//localhost:3000/node
+app.get('/node',(req,res)=>{
+    const explorer1={id:1, name:"Monserrat"}
+    const explorer2={id:2, name:"Monserrat"}
+    const explorer3={id:3, name:"Monserrat"}
+    res.send(explorer1,explorer2, explorer3)
+})
+
+//localhost:3000/explorers/monseeligo
+app.get('/explorers/:explorer',(req,res)=>{
+    console.log(req.params)
+    res.send(req.params)
+})
 
 app.listen(port,()=>{
     console.log("Server listo!!")
